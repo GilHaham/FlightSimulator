425659a4ccbe811c3c68a4290be8edee01196cfe
GilHaham <39674393+GilHaham@users.noreply.github.com>
2019-04-11 12:50:19 +0300

    TRY

--- FlightSimulator/ViewModels/BaseNotify.cs
+++ FlightSimulator/ViewModels/BaseNotify.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//check
 namespace FlightSimulator.ViewModels
 {
     public abstract class BaseNotify : INotifyPropertyChanged
